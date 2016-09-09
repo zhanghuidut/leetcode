ee7f4be7c3cc541587e3910d937c2b1178d48035
zhanghuidut <zhanghui_dut@163.com>
2016-09-09 23:34:18 +0800

    Update two_sum.py

--- 001_Two_Sum/two_sum.py
+++ 001_Two_Sum/two_sum.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # method 1
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -8,4 +9,28 @@ class Solution(object):
         for index1, val in enumerate(nums):
             if target - val in nums[index1+1:]:
                 return [index1, nums.index(target - val, index1+1)]
+        return [-1, -1]
+
+    # method 2: binary search
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        test = [(index, val) for index, val in enumerate(nums)]
+        test.sort(lambda x, y: cmp(x[1], y[1]))
+        
+        start = 0
+        end = len(test) -1
+        while 1:
+            if start == end:
+                break
+            val = test[start][1] + test[end][1]
+            if val == target:
+                return [test[start][0], test[end][0]]
+            elif val < target:
+                start += 1
+            else:
+                end -= 1
         return [-1, -1]
