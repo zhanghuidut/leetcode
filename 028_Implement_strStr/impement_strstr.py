8367608112c34937b017bf9a41bd23bc8c6194e2
zhanghuidut <zhanghui_dut@163.com>
2016-10-04 23:10:44 +0800

    Create impement_strstr.py

---
+++ 028_Implement_strStr/impement_strstr.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        len1, len2 = len(haystack), len(needle)
+        i, j = 0, 0
+        while 1:
+            if j == len2:
+                return i-len2
+            if i == len1:
+                return -1
+
+            if haystack[i] == needle[j]:
+                j += 1
+                i += 1
+            else:
+                i = i - j + 1
+                j = 0
