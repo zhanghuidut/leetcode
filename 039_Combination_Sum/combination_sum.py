3bc7c0dbdc004b1a149e90340edd38935d67fb1e
zhanghuidut <zhanghui_dut@163.com>
2016-10-23 10:03:32 +0800

    Update combination_sum.py

--- 039_Combination_Sum/combination_sum.py
+++ 039_Combination_Sum/combination_sum.py
@@ -22,4 +22,38 @@ class Solution(object):
         return res
  
 # 迭代
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        n, res = len(candidates), []
+        nums = [(target/item, item) for item in candidates]
+        
+        dep = 0
+        index = [-1]*n
+        path = []
+        while dep > -1:
+            index[dep] += 1
+            if index[dep] <= nums[dep][0]:
+                temp = path + [nums[dep][1]]*index[dep]
+                if sum(temp) == target:
+                    res.append(temp)
+                elif sum(temp) < target:
+                    if dep < n-1:
+                        dep += 1
+                        index[dep] = -1
+                        path = temp
+                else:
+                    dep -= 1
+                    if index[dep] != 0:
+                        path = path[:(-index[dep])]
+            else:
+                dep -= 1
+                if index[dep] != 0:
+                    path = path[:(-index[dep])]
+
+        return res
 
