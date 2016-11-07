b6bf33d2c4a670dce004cae43cc97bf8997ef368
zhanghuidut <zhanghui_dut@163.com>
2016-11-07 22:56:25 +0800

    Update remove_duplicates_from_sorted_array_II.py

--- 080_Remove_Duplicates_from_Sorted_Array_II/remove_duplicates_from_sorted_array_II.py
+++ 080_Remove_Duplicates_from_Sorted_Array_II/remove_duplicates_from_sorted_array_II.py
@@ -4,17 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        k, n = 0, 1
-        for i in xrange(1, len(nums)):
-            if n < 2 and nums[k] == nums[i]:
-                k += 1
-                n += 1
-                nums[k] = nums[i]
-            elif nums[k] != nums[i]:
+        k, n = -1, 1
+        for i in xrange(len(nums)):
+            if k == -1 or nums[k] != nums[i]:
                 k += 1
                 n = 1
+                nums[k] = nums[i]
+            elif n < 2 and nums[k] == nums[i]:
+                k += 1
+                n += 1
                 nums[k] = nums[i]
 
         return k+1
