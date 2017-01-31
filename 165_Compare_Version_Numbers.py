95ae0715d8f64178d2e6b4a0b47ba4be6c71400c
zhanghuidut <zhanghui_dut@163.com>
2017-01-31 10:55:05 +0800

    Update 165_Compare_Version_Numbers.py

--- 165_Compare_Version_Numbers.py
+++ 165_Compare_Version_Numbers.py
@@ -21,3 +21,30 @@ class Solution(object):
                 return -1
         return 0
 
+
+int compareVersion(char* version1, char* version2) {
+    char *ver1, *ver2;
+    int len1, len2, maxlen;
+    ver1 = strtok(version1, ".");
+    ver2 = strtok(version2, ".");
+
+    while(ver1 != NULL && ver2 != NULL)
+    {
+        int num1 = (int)ver1;
+        int num2 = (int)ver2;
+        if(num1 > num2)
+            return 1;
+        else if(num1 < num2)
+            return -1;
+
+        ver1 = strtok(NULL, ".");
+        ver2 = strtok(NULL, ".");
+    }
+    
+    if(ver1 == NULL)
+        return -1;
+    if(ver2 == NULL)
+        return 1;
+    
+    return 0;
+}
